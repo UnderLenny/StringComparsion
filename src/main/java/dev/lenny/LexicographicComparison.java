e8fc76451cbc3398273579b8b2463e47419d8f2f
lenny <leonid.gevorgyan42@gmail.com>
2024-05-05 20:30:54 +0700

    fix: исправил баг со сравнением строк одинаковой длины

--- src/main/java/dev/lenny/LexicographicComparison.java
+++ src/main/java/dev/lenny/LexicographicComparison.java
@@ -1,26 +1,23 @@
 package dev.lenny;
 
 public class LexicographicComparison {
-  public static int compareStrings(String firstString, String secondString) {
+  public int compareStrings(String firstString, String secondString) {
     int lengthComparison = Integer.compare(firstString.length(), secondString.length());
     if (lengthComparison != 0) {
       return lengthComparison;
     }
 
-    // Алгоритм лексикографического сравнения
-    for (int i = 0; i < firstString.length(); i++) {
+    int minLength = Math.min(firstString.length(), secondString.length());
+
+    for (int i = 0; i < minLength; i++) {
       char charFirst = firstString.charAt(i);
       char charSecond = secondString.charAt(i);
 
       if (charFirst != charSecond) {
-        return charFirst - charSecond;
-
+        return charFirst - charSecond > 0 ? 1 : -1;
       }
     }
 
     return 0;
   }
 }
-
-//    int result = firstString.compareTo(secondString);
-//    return result;
--- src/main/java/dev/lenny/Main.java
+++ src/main/java/dev/lenny/Main.java
@@ -3,11 +3,10 @@ package dev.lenny;
 public class Main {
   public static void main(String[] args) {
     LexicographicComparison comparison = new LexicographicComparison();
-    String first = "dasdasd";
-    String second = "gfhgfh";
-    int comparisonResult = LexicographicComparison.compareStrings(first, second);
+    String first = "hello";
+    String second = "hallo";
+    int comparisonResult = comparison.compareStrings(first, second);
 
-//    int result = comparison.compareStrings(first, second);
     System.out.println("Результат сравнения: " + comparisonResult);
 
   }
