e033dbc92d02452992ddef9711f1dacc3b1e794c
lenny <leonid.gevorgyan42@gmail.com>
2024-05-11 14:37:34 +0700

    установка стандарных значений

--- src/main/java/tests/CompareToTester.java
+++ src/main/java/tests/CompareToTester.java
@@ -21,7 +21,7 @@ public class CompareToTester {
 
     for (int i = 1; i <= 1000; i++) {
       // Генерация двух случайных строк
-      String str1 = generator.randomString (20, false, true);
+      String str1 = generator.randomString (20, true, true);
       String str2 = generator.randomString (20, true, true);
 
       // Сравнение с использованием пользовательского метода(актуальный результат)
